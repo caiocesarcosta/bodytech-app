891dadb3f80e3b8d418918a39e2adb39a0b73176
caiocesarcosta <kaiocesarcosta@gmail.com>
2025-03-25 15:36:56 -0300

    create brach login

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("com.android.application") version "8.9.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
     id("com.google.devtools.ksp") version "1.9.21-1.0.15" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
 
 
 }
